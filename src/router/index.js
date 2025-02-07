c1915b9308fdc6caf6019e1e645e9dec76fc9562
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-07 14:04:30 +0800

    login

--- src/router/index.js
+++ src/router/index.js
@@ -69,15 +69,12 @@ const router = createRouter({
 // 路由守衛：登入者不可訪問 login
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore();
-  console.log("Checking auth status:", authStore.isLoggedIn); // 確認登入狀態
 
-  // 如果已登入者訪問 Login 或 Register 頁面，重定向到 NotFound
-  if ((to.name === "Login" || to.name === "Register") && authStore.isLoggedIn) {
-    console.log("已登入，跳轉到 NotFound 頁面");
-    next({ name: "NotFound" }); // 已登入者跳轉到 NotFound 頁面
+  if (to.meta.requiresGuest && authStore.isLoggedIn) {
+    console.log("已登入，跳轉到首頁");
+    next("/"); // 防止已登入者進入登入頁
   } else {
-    console.log("Navigating normally");
-    next(); // 其他情況正常跳轉
+    next();
   }
 });
 
