975e8f0ba226ae780c29175da6018c9be838edad
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-07 14:44:21 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { useAuthStore } from "./stores/authStore"; // 引入 authStore
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-app.use(createPinia());
+// 初始化 Pinia 並掛載到 Vue 應用
+const pinia = createPinia();
+app.use(pinia);
+
+// 檢查登入狀態
+const authStore = useAuthStore();
+authStore.checkLoginStatus(); // 在應用啟動時檢查登入狀態
+
 app.use(router);
 
 app.mount("#app");
