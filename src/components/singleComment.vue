3e7cdda3e3adbe61da75b3a4fd2ee97698d08c83
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-09 18:34:08 +0800

    loading

--- src/components/singleComment.vue
+++ src/components/singleComment.vue
@@ -25,7 +25,7 @@ const fetchComments = async () => {
         timestamp: new Date(comment.created_at),
         file_url: comment.file_url, // 如有顯示
       }));
-      emit("loaded"); // 確保在數據成功獲取後再觸發事件
+      emit("loaded"); // 資料成功後再觸發 "loaded"
     } else {
       alert("無法獲取留言，數據格式不正確");
     }
