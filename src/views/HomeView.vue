0e952b5b5a820b724f4d69e62e29c0b960eae685
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-09 15:57:40 +0800

    loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,41 @@
 <template>
-  <n-space>
+  <n-space v-if="isLoading">
     <n-spin size="large" />
-    <div class="container-box">
-      <h1 class="page-title">此網站將不斷更新...</h1>
-      <div class="container">
-        <!-- 新增留言按鈕 -->
-        <!-- <button @click="goToMessagePage" class="add-comment-btn">新增留言</button> -->
+  </n-space>
+  <div class="container-box">
+    <h1 class="page-title">此網站將不斷更新...</h1>
+    <div class="container">
+      <!-- 新增留言按鈕 -->
+      <!-- <button @click="goToMessagePage" class="add-comment-btn">新增留言</button> -->
 
-        <div v-if="aru" class="aru">
-          <h1>最新留言</h1>
-          <div
-            v-for="(message, index) in socketStore.messages"
-            :key="index"
-            class="comment"
-          >
-            <h3 class="comment-title">{{ message.data.title }}</h3>
-            <p class="comment-content">{{ message.data.content }}</p>
-            <!-- <div class="comment-meta">
+      <div v-if="aru" class="aru">
+        <h1>最新留言</h1>
+        <div
+          v-for="(message, index) in socketStore.messages"
+          :key="index"
+          class="comment"
+        >
+          <h3 class="comment-title">{{ message.data.title }}</h3>
+          <p class="comment-content">{{ message.data.content }}</p>
+          <!-- <div class="comment-meta">
           <span class="comment-author">貼文者: {{ comment.name }}</span>
           <span class="comment-time"
             >貼文時間: {{ formatDate(comment.timestamp) }}</span
           >
         </div> -->
-            <!-- <p v-if="comment.file_url" class="comment-file">
+          <!-- <p v-if="comment.file_url" class="comment-file">
           附件: <a :href="comment.file_url" target="_blank">下載</a>
         </p>
         <button @click="goToCommentPage(comment.id)" class="view-button">
           查看及回覆
         </button> -->
-          </div>
         </div>
-
-        <singleComment />
       </div>
+
+      <singleComment @loaded="isLoading = false" />
     </div>
-    <Navbar />
-  </n-space>
+  </div>
+  <Navbar />
 </template>
 
 <script setup>
@@ -44,9 +44,10 @@ import { computed, onMounted } from "vue";
 import { useSocketStore } from "../stores/socketStore";
 import singleComment from "../components/singleComment.vue";
 import Navbar from "../components/Navbar.vue";
-const socketStore = useSocketStore();
 import { useRouter } from "vue-router";
+const socketStore = useSocketStore();
 const router = useRouter();
+const isLoading = ref(true); // 預設為 true，等 singleComment 加載完畢後變 false
 
 // 計算是否有新留言
 const aru = computed(() => socketStore.messages.length > 0);
