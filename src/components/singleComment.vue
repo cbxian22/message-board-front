6384207dec3553ed24a89294bebf55e3edc742e0
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-17 15:15:38 +0800

    add upload

--- src/components/singleComment.vue
+++ src/components/singleComment.vue
@@ -13,7 +13,7 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 const comments = ref([]);
 const emit = defineEmits();
-const commentImages = ref([]);
+// const commentImages = ref([]);
 const authStore = useAuthStore();
 authStore.checkLoginStatus();
 
@@ -82,6 +82,18 @@ const fetchComments = async () => {
         file_url: comment.file_url, // 如有顯示
       }));
       emit("loaded");
+
+      // 加載完留言後，再對圖片進行檢查
+      comments.value.forEach((comment, index) => {
+        const img = document.querySelector(`#comment-img-${comment.id}`);
+        if (img) {
+          img.onload = () => {
+            if (img.naturalHeight > img.naturalWidth) {
+              img.classList.add("tall-img"); // 直向圖片加上類別
+            }
+          };
+        }
+      });
     } else {
       alert("無法獲取留言，數據格式不正確");
     }
@@ -141,15 +153,15 @@ onMounted(() => {
 });
 
 // 確保對每一個圖片都加載後進行判斷
-onMounted(() => {
-  commentImages.value.forEach((img) => {
-    img.onload = () => {
-      if (img.naturalHeight > img.naturalWidth) {
-        img.classList.add("tall-img"); // 直向圖片加上類別
-      }
-    };
-  });
-});
+// onMounted(() => {
+//   commentImages.value.forEach((img) => {
+//     img.onload = () => {
+//       if (img.naturalHeight > img.naturalWidth) {
+//         img.classList.add("tall-img"); // 直向圖片加上類別
+//       }
+//     };
+//   });
+// });
 </script>
 
 <!-- <script setup>
@@ -350,7 +362,7 @@ onMounted(() => {
         <img
           :src="comment.file_url"
           alt="comment.file_url"
-          ref="commentImages"
+          :id="'comment-img-' + comment.id"
         />
       </span>
 
@@ -521,9 +533,8 @@ li:hover {
 }
 /* 直向 */
 .tall-img {
-  width: 40%; /* 控制圖片寬度 */
-  max-height: 200px; /* 限制圖片的最大高度 */
-  object-fit: contain; /* 保持圖片比例並完整顯示 */
+  width: 55%;
+  height: auto;
 }
 
 .dark-mode .modal-overlay {
