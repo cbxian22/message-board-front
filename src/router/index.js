20ef8c74b05be27e4e48e3ca3c32876bdbb6b90a
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-18 19:37:00 +0800

    profile

--- src/router/index.js
+++ src/router/index.js
@@ -80,26 +80,9 @@ const router = createRouter({
 // });
 
 // 如果已登入者訪問 Login 或 Register 頁面，重定向到首頁
-// router.beforeEach(async (to, from, next) => {
-//   const authStore = useAuthStore();
-//   const username = to.params.username;
-//   // 等待 Vue 完成所有更新（包括 Pinia 狀態）後再進行導航
-//   await nextTick();
-
-//   console.log("Checking auth status:", authStore.isLoggedIn); // 確認最新的登入狀態
-
-//   // 如果已登入且正在訪問 Login 或 Register 頁面，重定向到首頁
-//   if ((to.name === "Login" || to.name === "Register") && authStore.isLoggedIn) {
-//     console.log("已登入，跳轉到首頁");
-//     return next({ name: "Home" }); // 已登入的使用者跳轉到首頁
-//   }
-
-//   next(); // 其他情況正常導航
-// });
-
 router.beforeEach(async (to, from, next) => {
   const authStore = useAuthStore();
-
+  const username = to.params.username;
   // 等待 Vue 完成所有更新（包括 Pinia 狀態）後再進行導航
   await nextTick();
 
@@ -111,25 +94,7 @@ router.beforeEach(async (to, from, next) => {
     return next({ name: "Home" }); // 已登入的使用者跳轉到首頁
   }
 
-  // 如果用戶點擊了自己的 Profile 頁面，我們應該直接停留在當前頁面
-  // 如果當前頁面是用戶的頁面，不做任何跳轉
-  if (authStore.isLoggedIn && to.params.username === authStore.userName) {
-    console.log("已在自己的頁面，無需跳轉");
-    return next(); // 讓頁面保持在當前頁面
-  }
-
-  // 否則，跳轉到自己的頁面
-  if (
-    authStore.isLoggedIn &&
-    to.name !== "Profile" &&
-    to.params.username !== authStore.userName
-  ) {
-    console.log("跳轉到自己的頁面");
-    return next(`/@${authStore.userName}`); // 跳轉到自己的頁面
-  }
-
-  // 繼續導航
-  next();
+  next(); // 其他情況正常導航
 });
 
 export default router;
