bcca7eaa7ddf48877af56d6ef863e5f41d856bb7
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-18 19:47:10 +0800

    profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref, computed, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router"; // 引入 useRoute;
 import axios from "axios";
@@ -29,6 +29,51 @@ onMounted(async () => {
     router.replace("/not-found");
   }
 });
+</script> -->
+<script setup>
+import { ref, computed, onMounted, watch } from "vue";
+import { useRoute, useRouter } from "vue-router"; // 引入 useRoute;
+import axios from "axios";
+
+import selfSingleComment from "../components/selfSingleComment.vue";
+import Navbar from "../components/Navbar.vue";
+import NavbarUp from "../components/NavbarUp.vue";
+
+// 計算是否有新留言
+const aru = computed(() => socketStore.messages.length > 0);
+
+const router = useRouter();
+const route = useRoute();
+const username = ref(route.params.username); // 使用 ref 存儲 username
+
+// 初始載入時獲取資料
+onMounted(async () => {
+  await fetchUserData(username.value);
+});
+
+// 監控 route.params.username 變化，重新獲取資料
+watch(
+  () => route.params.username,
+  async (newUsername) => {
+    username.value = newUsername;
+    await fetchUserData(username.value);
+  }
+);
+
+// 獲取使用者資料
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(
+      `https://message-board-server-7yot.onrender.com/api/users/${username}`
+    );
+    if (response.data.message === "使用者不存在") {
+      router.replace("/not-found");
+    }
+  } catch (error) {
+    console.error("查詢用戶錯誤:", error);
+    router.replace("/not-found");
+  }
+};
 </script>
 
 <template>
@@ -58,13 +103,12 @@ onMounted(async () => {
   display: flex;
   flex-direction: column;
   justify-self: center;
-  /* 預留空間，避免被固定的 Navbar 擋住 */
   margin: 100px 0;
 }
 
 .container {
   border: 0.5px solid #373737;
   border-radius: 30px;
-  overflow: hidden; /* 防止背景超出圓角範圍 */
+  overflow: hidden;
 }
 </style>
