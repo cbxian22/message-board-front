3a5ba0086ffb8ce34cb36d5c2a81415e5fff2f60
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-20 23:23:19 +0800

    info

--- src/components/selfInfo.vue
+++ src/components/selfInfo.vue
@@ -30,8 +30,10 @@ const fileUrl = ref(null);
 const fileInputRef = ref(null);
 
 // 獲取 user 資料
-const fetchInfo = async (username) => {
-  const username = router.currentRoute.value.params.username; // 這裡重新獲取 username
+const fetchInfo = async (
+  username = router.currentRoute.value.params.username
+) => {
+  // const username = router.currentRoute.value.params.username; // 這裡重新獲取 username
   try {
     const response = await axios.get(
       `https://message-board-server-7yot.onrender.com/api/users/${username}`
