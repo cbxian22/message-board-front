614151a3266dbd38ab473bc304a959931fa59282
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-21 15:55:11 +0800

    info

--- src/stores/axiosConfig.js
+++ src/stores/axiosConfig.js
@@ -1,19 +1,45 @@
-import axios from "axios";
+// import axios from "axios";
 
-// 每次请求前检查 localStorage 的 token
-const setAuthorizationHeader = () => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  } else {
-    delete axios.defaults.headers.common["Authorization"]; // 没有 token 时，删除 Authorization
-  }
-};
+// // 每次请求前检查 localStorage 的 token
+// const setAuthorizationHeader = () => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+//   } else {
+//     delete axios.defaults.headers.common["Authorization"]; // 没有 token 时，删除 Authorization
+//   }
+// };
+
+// // 确保每次请求前都设置 Authorization 头
+// axios.interceptors.request.use(
+//   (config) => {
+//     setAuthorizationHeader();
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default axios;
+
+import axios from "axios";
+import { useAuthStore } from "../stores/authStore"; // 引入 Pinia 的 authStore
+
+// 创建自定义的 axios 实例
+const apiClient = axios.create({
+  baseURL: "https://message-board-server-7yot.onrender.com/api", // 设置基础 URL
+  headers: { "Content-Type": "application/json" }, // 设置默认头部
+});
 
 // 确保每次请求前都设置 Authorization 头
-axios.interceptors.request.use(
+apiClient.interceptors.request.use(
   (config) => {
-    setAuthorizationHeader();
+    const authStore = useAuthStore();
+    const token = authStore.token;
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
@@ -21,4 +47,4 @@ axios.interceptors.request.use(
   }
 );
 
-export default axios;
+export default apiClient;
--- src/stores/usePostStore.js
+++ src/stores/usePostStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import axios from "../stores/axiosConfig"; // 確保你的 axios 設置正確
+import apiClient from "../stores/axiosConfig"; // 確保你的 axios 設置正確
 
 export const usePostStore = defineStore("post", {
   state: () => ({
@@ -10,12 +10,10 @@ export const usePostStore = defineStore("post", {
     // 刪除貼文
     async deletePost(postId, userId) {
       try {
-        const response = await axios.delete(`/posts/${postId}/${userId}`);
-        this.posts = this.posts.filter((post) => post.id !== postId);
-        return response.data.message;
+        const response = await apiClient.delete(`/posts/${postId}/${userId}`);
+        console.log("刪除成功", response.data);
       } catch (error) {
         console.error("刪除貼文失敗", error);
-        throw error;
       }
     },
   },
