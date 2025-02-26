bd26be32ad25ca38ae4a09759a189859435cafb3
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-26 10:32:34 +0800

    position

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "@/stores/authStore";
+import { useScrollStore } from "@/stores/scrollStore";
 import { nextTick } from "vue";
 import { useScrollStore } from "@/stores/scrollStore";
 import HomeView from "../views/HomeView.vue";
@@ -11,26 +12,17 @@ const router = createRouter({
       path: "/",
       name: "Home",
       component: HomeView,
-      // meta: { requiresAuth: true },
     },
     {
       path: "/login",
       name: "Login",
       component: () => import("../views/LoginView.vue"),
-      // meta: { requiresGuest: true },
     },
     {
       path: "/register",
       name: "Register",
       component: () => import("../views/Register.vue"),
-      // meta: { requiresGuest: true },
     },
-    // {
-    //   path: "/chat",
-    //   name: "Chat",
-    //   component: () => import("../views/ChatView.vue"),
-    //   // meta: { requiresGuest: true },
-    // },
     {
       path: "/@:username",
       name: "Profile",
@@ -41,8 +33,7 @@ const router = createRouter({
       path: "/post/:id",
       name: "Post",
       component: () => import("../views/PostView.vue"),
-      props: true, // 允許將路由參數作為 props 傳遞
-      // meta: { requiresGuest: true },
+      props: true,
     },
 
     {
@@ -75,27 +66,6 @@ const router = createRouter({
     return { top: 0, behavior: "smooth" };
   },
 });
-
-// 路由守衛：進入頁面前檢查登入狀態
-// 已開放為登入者瀏覽，所以註解！！！
-// router.beforeEach((to, from, next) => {
-//   const authStore = useAuthStore();
-//   console.log("Navigating to:", to.path); // 顯示正在跳轉的路徑
-//   console.log("Route meta:", to.meta); // 顯示路由的 meta 設置
-
-//   // 如果需要登入的頁面且未登入
-//   if (to.meta.requiresAuth && !authStore.isLoggedIn) {
-//     console.log("未登入，跳轉到 /login");
-//     next("/login"); // 如果未登入，跳轉到登入頁
-//   }
-//   // 如果需要未登入用戶訪問的頁面，且用戶已登入
-//   else if (to.meta.requiresGuest && authStore.isLoggedIn) {
-//     console.log("已登入，跳轉到首頁");
-//     next("/"); // 如果已登入，跳轉到首頁
-//   } else {
-//     next(); // 其他情況，繼續進行導航
-//   }
-// });
 
 router.beforeEach(async (to, from, next) => {
   const authStore = useAuthStore();
