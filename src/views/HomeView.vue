acb9f4ac970e85de888e3e08a280d4e9d2bbc79f
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-26 09:45:57 +0800

    position

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,17 +61,22 @@ const socketStore = useSocketStore();
 const isLoading = ref(true);
 
 const saveScrollPosition = () => {
-  const position = window.scrollY || document.documentElement.scrollTop;
-  scrollStore.setScrollPosition(position);
+  scrollStore.setScrollPosition(window.scrollY);
 };
 
 onMounted(() => {
+  // 監聽滾動事件，記錄滾動位置
   window.addEventListener("scroll", saveScrollPosition);
+
+  // 回到先前儲存的位置
+  nextTick(() => {
+    window.scrollTo(0, scrollStore.getScrollPosition());
+  });
 });
 
 onUnmounted(() => {
+  // 離開頁面時移除事件監聽
   window.removeEventListener("scroll", saveScrollPosition);
-  saveScrollPosition(); // 離開前保存最後位置
 });
 
 // 當 singleComment 加載完成時，更新 isLoading
