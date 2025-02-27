cd33d4ae4f4e64c52bf4f488374849958b9edcfe
chenboxian <xian@chenboxiandeMacBook-Air.local>
2025-02-27 15:46:25 +0800

    logout

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -276,7 +276,9 @@ const updateWidth = () => {
       </n-drawer-content>
     </n-drawer>
     <!--  -->
-    <div v-show="info.is_private">這是私人帳號，加入好友即可查看對方貼文。</div>
+    <div v-show="info.is_private == 1">
+      這是私人帳號，加入好友即可查看對方貼文。
+    </div>
   </div>
 
   <!-- 登入 Modal -->
